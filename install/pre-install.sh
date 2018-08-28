948a990ff3fdf08bba06843f1e4b92b09a9e6665
Josiah <josiah.hanna@utexas.edu>
2018-08-28 18:51:15 -0500

    Updated pre-install, lab_setup script, and lab machine set-up scripts.

--- install/pre-install.sh
+++ install/pre-install.sh
@@ -1,5 +1,14 @@
 # Install a dependency for the install script
 
+# Install git
+sudo apt-get install git
+
+# Install vim
+sudo apt-get install vim
+
+# Install terminator
+sudo apt-get install terminator
+
 # Install pip
 sudo apt-get install python-pip
 
