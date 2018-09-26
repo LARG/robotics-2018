0b00084d2b6dbb13f8e2e36e93171129bb3b712e
Josiah <josiah.hanna@utexas.edu>
2018-09-26 11:04:14 -0500

    Initialized a value in robot state block.

--- core/memory/RobotStateBlock.cpp
+++ core/memory/RobotStateBlock.cpp
@@ -4,6 +4,7 @@ RobotStateBlock::RobotStateBlock() : WO_SELF(5), team_(TEAM_BLUE), role_(WO_SELF
   header.version = 6;
   header.size = sizeof(RobotStateBlock);
   robot_id_ = 0;
+  robot_id_ = -1;
   team_changed_ = true;
   ignore_comms_ = false;
   strncpy(reinterpret_cast<char*>(body_id_.data()), "<NO BODY ID SET>", body_id_.size());
