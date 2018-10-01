6eedb1009e930ae300fc530a5542babe60cb04e4
Josiah <josiah.hanna@utexas.edu>
2018-10-01 10:10:45 -0500

    Load kick on demand instead of when kick module initializes.

--- core/python/init.py
+++ core/python/init.py
@@ -82,7 +82,7 @@ def processBehaviorFrame():
     """Run loaded behavior processFrame."""
     global behaviorLoaded
     if not behaviorLoaded:
-        runBehavior("sample")
+        runBehavior("walk_kick")
         behaviorLoaded = True
     try:
         if memory.robot_state.WO_SELF != core.WO_TEAM_COACH:
